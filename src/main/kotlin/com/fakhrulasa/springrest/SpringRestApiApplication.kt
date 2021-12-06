a3a848ad8f46a2cb974172d0566a91ef572d86e4
fakhrulasa <fakhrulasa@gmail.com>
2021-12-06 16:13:52 +0600

    rest api

---
+++ src/main/kotlin/com/fakhrulasa/springrest/SpringRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.fakhrulasa.springrest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringRestApiApplication>(*args)
+}

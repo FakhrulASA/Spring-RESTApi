22ba870fab447e4984733d7eb83c9a2e31ff79d3
fakhrulasa <fakhrul.alam@btracsl.com>
2021-12-12 13:28:20 +0600

    - jdk update

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "17"
 	}
 }
 
--- src/main/kotlin/com/fakhrulasa/springrest/controller/NoteController.kt
+++ src/main/kotlin/com/fakhrulasa/springrest/controller/NoteController.kt
@@ -1,4 +1,14 @@
 package com.fakhrulasa.springrest.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
 class NoteController {
+    @GetMapping
+    fun helloWorld():String{
+        return "Hello World"
+    }
 }
